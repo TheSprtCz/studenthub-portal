12b6c46d641f5d12163ac22f8a48bba7a2ce6279
Stefan Bunciak <pipistik.bunciak@gmail.com>
2017-01-15 17:20:49 +0100

    Configure Hibernate bundle

--- src/main/java/cz/studenthub/StudentHubApplication.java
+++ src/main/java/cz/studenthub/StudentHubApplication.java
@@ -1,29 +1,52 @@
 package cz.studenthub;
 
+import cz.studenthub.core.Company;
+import cz.studenthub.core.Faculty;
+import cz.studenthub.core.Topic;
+import cz.studenthub.core.TopicApplication;
+import cz.studenthub.core.University;
+import cz.studenthub.core.User;
 import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
+/**
+ * Dropwizard application entry point.
+ * 
+ * @author sbunciak
+ * @since 1.0
+ */
 public class StudentHubApplication extends Application<StudentHubConfiguration> {
 
-    public static void main(final String[] args) throws Exception {
-        new StudentHubApplication().run(args);
-    }
-
+  private final HibernateBundle<StudentHubConfiguration> hibernate = new HibernateBundle<StudentHubConfiguration>(
+      // list of entities
+      User.class, Topic.class, TopicApplication.class, Company.class, University.class, Faculty.class) {
+    
     @Override
-    public String getName() {
-        return "StudentHub";
+    public DataSourceFactory getDataSourceFactory(StudentHubConfiguration configuration) {
+      return configuration.getDataSourceFactory();
     }
+  };
 
-    @Override
-    public void initialize(final Bootstrap<StudentHubConfiguration> bootstrap) {
-        // TODO: application initialization
-    }
+  public static void main(final String[] args) throws Exception {
+    new StudentHubApplication().run(args);
+  }
 
-    @Override
-    public void run(final StudentHubConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
-    }
+  @Override
+  public String getName() {
+    return "Student Hub";
+  }
+
+  @Override
+  public void initialize(final Bootstrap<StudentHubConfiguration> bootstrap) {
+    bootstrap.addBundle(hibernate);
+  }
+
+  @Override
+  public void run(final StudentHubConfiguration configuration, final Environment environment) {
+    // TODO: implement application
+  }
 
 }
